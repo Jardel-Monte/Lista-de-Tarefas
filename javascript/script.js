46100e02035f82e5de21ed374eb2eda2cd69bbc8
Jardel-Monte <141651306+Jardel-Monte@users.noreply.github.com>
2023-10-23 10:37:29 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,29 +1,42 @@
 // Função para adicionar uma nova tarefa
 function novaTarefa() {
-    const tarefaInput = document.getElementById("tarefa");
-    const tarefaTexto = tarefaInput.value.trim();
+  const tarefaInput = document.getElementById("tarefa");
+  const tarefaTexto = tarefaInput.value.trim();
 
-    if (tarefaTexto === "") {
-        alert("Por favor inserir tarefa desejada.");
-        return;
-    }
+  if (tarefaTexto === "") {
+    alert("Por favor inserir tarefa desejada.");
+    return;
+  }
 
-    const tarefaLista = document.getElementById("tarefa-lista");
+  const tarefaLista = document.getElementById("tarefa-lista");
 
-    const li = document.createElement("li");
-    li.innerHTML = `
-        <input type="checkbox" id="checkbox">
-        <span>${tarefaTexto}</span>
-        <button class="remover" onclick="removerTarefa(this)">Remover</button>
-    `;
+  const li = document.createElement("li");
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  const span = document.createElement("span");
+  span.textContent = tarefaTexto;
+  const removerButton = document.createElement("button");
+  removerButton.classList.add("remover");
+  removerButton.textContent = "X";
 
-    tarefaLista.appendChild(li);
 
-    tarefaInput.value = ""; // Limpar a caixa de input
+
+  li.appendChild(checkbox);
+  li.appendChild(span);
+  li.appendChild(removerButton);
+
+  tarefaLista.appendChild(li);
+
+  tarefaInput.value = ""; // Limpar a caixa de input
+
+// Adicionar um event listener para o botão "remover" para remover a tarefa
+    removerButton.addEventListener("click", function () {
+        removerTarefa(li);
+    });
 }
 
-// Função para remover uma tarefa
-function removerTarefa(button) {
-    const tarefaLista = document.getElementById("tarefa-lista");
-    tarefaLista.removeChild(button.parentElement);
+// Função para remover a tarefa desejada
+function removerTarefa(li) {
+  const tarefaLista = document.getElementById("tarefa-lista");
+  tarefaLista.removeChild(li);
 }
